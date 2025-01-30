bd733144f3d83cb94fb635664a2421babb5fe5a5
Camila <mendemmcamila@gmail.com>
2025-01-30 18:12:45 -0300

    feat: create custom error classes

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { RegisterUseCase } from '@/use-cases/register'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -19,8 +20,9 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   try {
     await registerUseCase.execute({ email, name, password })
   } catch (err) {
-    return reply.status(409).send()
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+    return reply.status(201).send() // TODO: fix me
   }
-
-  return reply.status(201).send()
 }
